0603de66357a57f1122ebc66d31cf194c5593123
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-02-02 18:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-//complete this code
-class Person {}
+// Person class
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    
+    get name() {
+        return this._name;
+    }
+    
+    set age(age) {
+        this._age = age;
+    }
+}
 
-class Student extends Person {}
+// Student subclass
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
 
-class Teacher extends Person {}
+// Teacher subclass
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
